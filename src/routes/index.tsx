10215603fb5579edf2da0f8cdadd96178c525fc7
Rajit Khanna <rajitskhanna@gmail.com>
2025-07-29 09:39:48 -0700

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute } from "@tanstack/react-router";
+export const Route = createFileRoute("/")({
+  component: Home,
+});
+
+function Home() {
+  return (
+    <div className="p-2">
+      <h3>Rajit Khanna</h3>
+    </div>
+  );
+}
